916c1eb50f82ea559712ec7848e2305415fae1ad
Mordokkai <sylvain.guy@phelma.grenoble-inp.fr>
2015-12-07 00:42:26 +0100

    transmission structures par socket

--- client/main_client.cpp
+++ client/main_client.cpp
@@ -15,15 +15,21 @@ typedef int SOCKET;
 typedef struct sockaddr_in SOCKADDR_IN;
 typedef struct sockaddr SOCKADDR;
 
+typedef struct adress{
+	char name[256];
+	char ad[256];
+} ADRESS;
+
 int main(void){
 	int sock_err;
-	
+
 	SOCKET sock;
 	SOCKADDR_IN sin;
-
+	char buffer[256]="";
+	ADRESS ad_recue;
 	// Création de la socket client
 	sock = socket(AF_INET, SOCK_STREAM, 0);
-	
+
 	// COnfiguration de la connexion
 	sin.sin_addr.s_addr = inet_addr("127.0.01");
 	sin.sin_family = AF_INET;
@@ -37,8 +43,16 @@ int main(void){
 	}
 	printf("Connexion à %s  sur le port %d \n", inet_ntoa(sin.sin_addr), htons(sin.sin_port));
 
+	// Attente de réception de données
+	sock_err = recv(sock, &ad_recue, sizeof(ad_recue), 0 );
+
+	if(sock_err ==SOCKET_ERROR){
+		printf("Problème lors de la réception du message\n");
+		exit(-1);
+	}
+	printf("Recu: %s %s\n", ad_recue.name, ad_recue.ad);
 	// Fermeture de la socket
 	close(sock);
-
+	getchar();
 	return EXIT_SUCCESS;
 }
--- serveur/main.cpp
+++ serveur/main.cpp
@@ -5,6 +5,7 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define INVALID_SOCKET -1
 #define SOCKET_ERROR -1
@@ -14,6 +15,12 @@
 typedef int SOCKET;
 typedef struct sockaddr_in SOCKADDR_IN;
 typedef struct sockaddr SOCKADDR;
+typedef struct adress{
+	char name[256];
+	char ad[256];
+} ADRESS;
+
+
 
 int main(void){
 	int sock_err;
@@ -21,7 +28,10 @@ int main(void){
 	SOCKADDR_IN sin;
 	SOCKET sock;
 	socklen_t sock_len_serv = sizeof(sin);
-
+	char message[256] = "Bonjour";
+	ADRESS ad;
+	strcpy(ad.name, "test");
+	strcpy(ad.ad, "mordokkai@gmail.com");
 	//Coté client
 	SOCKADDR_IN csin;
 	SOCKET csock;
@@ -58,13 +68,21 @@ int main(void){
 	csock = accept(sock,(SOCKADDR*)&csin, &sock_len_client); //Ceci est un appel bloquant
 	printf("Un client se connecte avec la socket %d de %s: %d\n", csock, inet_ntoa(csin.sin_addr),htons(csin.sin_port));
 
+	// Envoie d'un message au client
+	sock_err = send(csock, &ad, sizeof(ad), 0);
+	if(sock_err == SOCKET_ERROR){
+		printf("Problème lors de l'envoi du message du serveur vers le client\n");
+	exit(-1);
+	}
 	//Fermetures
 	printf("Fermeture de la socket client\n");
-	close(csock);
+	//close(csock);
+	shutdown(csock,2);
 
 	printf("Fermeture de la socket serveur\n");
 	close(sock);
-
+	close(csock);
+	getchar();
 	printf("Terminé\n");
 
 	return EXIT_SUCCESS;
